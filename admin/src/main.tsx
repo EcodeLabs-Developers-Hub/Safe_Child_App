b9bc451bbdf6573f4558a48035b7ee55b53a59de
eshun <72285159+kingsleyeshunmintah@users.noreply.github.com>
2026-09-09 06:17:26 +0100

    version branch migrate

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AdminApp } from './app';
+import { AdminApp } from './app.tsx';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AdminApp /></React.StrictMode>);
